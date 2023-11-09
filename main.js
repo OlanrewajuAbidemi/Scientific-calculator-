ef416275386aab465585272576bcceec58a1dcc3
USER <user@USERs-MacBook-Air.local>
2023-11-09 13:24:24 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById("shift").style.borderTopLeftRadius = "15px";
+document.getElementById("onbutton").style.borderTopRightRadius = "15px"
+document.getElementById("p&mbutton").style.borderBottomLeftRadius = "15px"
+document.getElementById("mode").style.fontSize = "8px"
+document.getElementById("shift").style.fontSize = "10px"
+document.getElementById("mrbutton").style.borderBottomRightRadius = "15px"
+document.getElementById("seven").style.borderTopLeftRadius = "15px";
+document.getElementById("ac").style.borderTopRightRadius = "15px"
+document.getElementById("ac").style.backgroundColor = "#F1C745"
+document.getElementById("c").style.backgroundColor = "#F1C745"
+document.getElementById("zero").style.borderBottomLeftRadius = "15px"
+document.getElementById("m").style.borderBottomRightRadius = "15px"
+ 
+
+
