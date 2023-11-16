cc0c8fd6424f07ee3069420e9e8ed8059b124e1d
USER <user@USERs-MacBook-Air.local>
2023-11-16 14:04:45 +0100

    that is all

--- main.js
+++ main.js
@@ -10,7 +10,38 @@ document.getElementById("ac").style.backgroundColor = "#F1C745"
 document.getElementById("c").style.backgroundColor = "#F1C745"
 document.getElementById("zero").style.borderBottomLeftRadius = "15px"
 document.getElementById("m").style.borderBottomRightRadius = "15px"
+function numadd(x){
+    document.getElementById("input").value += x
+}
+function cclear(){
+    document.getElementById("input").value= ""
 
+}
+function eevaluate(){
+    document.getElementById("input").value =  eval(document.getElementById("input").value)
+}
+const sq= (x) => {
+   document.getElementById("input").value =  x(document.getElementById("input").value)
+}
+function cos(x){
+    let anglein = Math.PI / 180
+  document.getElementById("input").value= x (  document.getElementById("input").value * anglein)
+}
+function poww(){
+    document.getElementById("input").value= Math.pow(document.getElementById("input").value,2)
+
+}
+const logg = () =>{
+    document.getElementById("input").value = Math.log10(document.getElementById("input").value)
+}
+
+function del(){
+
+}
+function On(){
+    document.getElementById("input").value = ""
+}
  
+
 
 
